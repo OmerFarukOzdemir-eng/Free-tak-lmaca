eba4adcc65f2d4eb5ee6e48143f4536aca29b564
ozdem <ozdem@CANAVAR>
2023-03-27 13:59:50 +0300

    Proje dosyası ekle.

---
+++ Free takılmaca.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int no1, no2, total;
+
+    cout << "Enter the 1st number:";
+    cin >> no1;
+    cout << "Enter the 2nd number:";
+    cin >> no2;
+
+    total = no1 + no2;
+    cout << "Sum of entered numbers:"<<total;
+}
